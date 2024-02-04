c452a44fe32725fb402d46416af404365d554e59
Ahmed Dinar <dinar@timetackle.com>
2024-02-05 00:38:51 +0600

    Final test

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/exeception-filters/all-exception-filter';
 
-/**
- * to trigger 2
- */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));

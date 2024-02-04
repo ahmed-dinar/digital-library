163f89ce893da4c2616921b826a422c0c1a226eb
Ahmed Dinar <dinar@timetackle.com>
2024-02-05 00:30:53 +0600

    trigger workflow

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/exeception-filters/all-exception-filter';
 
+/**
+ * to trigger
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));

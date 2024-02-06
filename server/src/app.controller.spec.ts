da7df035752dd96cdd1c955cec2c26c894172569
Ahmed Dinar <dinar@timetackle.com>
2024-02-07 00:22:03 +0600

    Update test

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello World! Welcome to Digital Library!"', () => {
       expect(appController.getHello()).toBe('Hello World! Welcome to Digital Library!');
     });
   });
